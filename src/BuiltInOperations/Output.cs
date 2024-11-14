9e08da5616c5ea0bd3a644c51071cb159b8a67c4
Domowy <Domowy@DOMOWY>
2024-11-14 22:26:44 +0100

    Output has now aboility to set log-level.

--- src/BuiltInOperations/Output.cs
+++ src/BuiltInOperations/Output.cs
@@ -7,12 +7,31 @@ public class Output(IOutput output) : Operation
     public override Dictionary<string, OperationParameter> Parameters { get; } = new Dictionary<string, OperationParameter>
     {
         { "text", new OperationParameter { Description = "Text to output", RequiredText = true } },
-        { "formatting", new OperationParameter { Description = "Optional formatting", AllowedValues = ["default", "raw"] } }
+        { "formatting", new OperationParameter { Description = "Optional formatting", AllowedValues = ["default", "raw"] } },
+        { "logLevel", new OperationParameter { Description = "Logging level", AllowedValues = ["trace", "debug", "information", "warning", "error"] } }
     };
   
     public override Task Run(CancellationToken cancellationToken)
     {
-        output.Result(Parameters["text"].Value.TextValue!, Parameters["formatting"].Value.TextValue);
+        switch (Parameters["logLevel"].Value.TextValue)
+        {
+            case "trace":
+                output.Trace(Parameters["text"].Value.TextValue!);
+                break;
+            case "debug":
+                output.Debug(Parameters["text"].Value.TextValue!);
+                break;
+            case "warning":
+                output.Warning(Parameters["text"].Value.TextValue!);
+                break;
+            case "error":
+                output.Error(Parameters["text"].Value.TextValue!);
+                break;
+            default:
+                output.Result(Parameters["text"].Value.TextValue!, Parameters["formatting"].Value.TextValue);
+                break;
+        }
+
         return Task.CompletedTask;
     }
 }
