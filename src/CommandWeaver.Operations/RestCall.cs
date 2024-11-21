f2103c1170dba6ce5343ad875af8ceec63767708
Domowy <Domowy@DOMOWY>
2024-11-21 21:09:29 +0100

    Working on RestCall.cs

--- src/CommandWeaver.Operations/RestCall.cs
+++ src/CommandWeaver.Operations/RestCall.cs
@@ -1,4 +1,6 @@
-public record RestCall(IConditionsService conditionsService, IVariables variables) : Operation
+using System.Text;
+
+public record RestCall(IConditionsService conditionsService, IVariables variables, ISerializerFactory serializerFactory) : Operation
 {
     public override string Name => nameof(RestCall);
 
@@ -8,43 +10,63 @@ public record RestCall(IConditionsService conditionsService, IVariables variable
         {"method", new OperationParameter { Description = "Operation to perform", RequiredText = true, AllowedValues = [HttpMethod.Get.ToString(), HttpMethod.Post.ToString(), HttpMethod.Put.ToString(), HttpMethod.Delete.ToString(), HttpMethod.Patch.ToString() ] } },
         {"headers", new OperationParameter { Description = "Metadata for the request"} },
         {"body", new OperationParameter { Description = "Data sent with the request"} },
-        {"timeout", new OperationParameter { Description = "How long to wait for a response before failing"} },
+        {"timeout", new OperationParameter { Description = "Seconds to wait for a response before failing"} },
         {"certificate", new OperationParameter { Description = "Path to certificate file"} },
         {"certificatePassword", new OperationParameter { Description = "Certificate password if any"} }
     };
     
     public override async Task Run(CancellationToken cancellationToken)
     {
+        var httpClient = new HttpClient();
+        AddHeaders(httpClient);
         using var request = new HttpRequestMessage();
         request.Method = HttpMethod.Parse(Parameters["method"].Value.TextValue!);
         request.RequestUri = new Uri(Parameters["url"].Value.TextValue!);
-        var headers = Parameters["headers"].Value.ListValue;
-        if (headers != null)
-            foreach (var header in headers)
-            {
-                var skipHeader = false;
-                string? name = null;
-                string? value = null;
-                foreach (var headerKey in header.Keys)
-                {
-                    if (string.Equals(headerKey, "conditions", StringComparison.OrdinalIgnoreCase))
-                    {
-                        var condition = conditionsService.GetFromDynamicValue(header[headerKey]);
-                        if (condition != null && conditionsService.ShouldBeSkipped(condition, variables))
-                            skipHeader = true;
-                    }
-                    else
-                    {
-                        name = headerKey;
-                        value = header[headerKey].TextValue;
-                    }
-                }
-                if (!skipHeader && name!=null)
-                    request.Headers.Add(name, value);
-            }
-        
-        var httpClient = new HttpClient();
+        httpClient.Timeout = TimeSpan.FromSeconds(Parameters["timeout"].Value.NumericValue ?? 60);
+        if (Parameters["body"].Value.TextValue != null)
+            request.Content = new StringContent(Parameters["body"].Value.TextValue!, Encoding.UTF8, "application/json");
+        if (Parameters["body"].Value.ObjectValue != null)
+        {
+            var serializer = serializerFactory.GetDefaultSerializer(out _);
+            serializer.TrySerialize(Parameters["body"].Value, out var body, out _);
+            request.Content = new StringContent(body, Encoding.UTF8, GetContentType() ?? "application/json");
+        }
+
         var result = await httpClient.SendAsync(request, cancellationToken);
         var t = await result.Content.ReadAsStringAsync(cancellationToken);
+    }
+
+    private string? GetContentType() => Parameters["headers"].Value.ListValue?.FirstOrDefault(h => string.Equals(h["key"]?.TextValue, "content-type", StringComparison.InvariantCultureIgnoreCase))?["value"].TextValue;
+
+    private void AddHeaders(HttpClient httpClient)
+    {
+        // var headers = Parameters["headers"].Value.ListValue;
+        // if (headers == null)
+        //     return;
+        //
+        // foreach (var header in headers)
+        // {
+        //     var skipHeader = false;
+        //     string? name = null;
+        //     string? value = null;
+        //     foreach (var headerKey in header.Keys)
+        //         if (string.Equals(headerKey, "conditions", StringComparison.OrdinalIgnoreCase))
+        //         {
+        //             var condition = conditionsService.GetFromDynamicValue(header[headerKey]);
+        //             if (condition != null && conditionsService.ShouldBeSkipped(condition, variables))
+        //                 skipHeader = true;
+        //         }
+        //         else
+        //         {
+        //             name = headerKey;
+        //             value = header[headerKey].TextValue;
+        //         }
+        //     
+        //     if (!skipHeader && name!=null)
+        //         if (!httpClient.DefaultRequestHeaders.TryAddWithoutValidation(name, value))
+        //         {
+        //             
+        //         }
+        // }
     }
 }
